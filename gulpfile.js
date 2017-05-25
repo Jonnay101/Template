c956263878911b29fc9665869046e1371152281a
Jonnay101 <john.hughes101@gmail.com>
2017-05-25 23:33:01 +0100

    Gulp Updates
    Changed old plugins for newer versions.
    Changed to connect server from livereload.
    Move html files to src folder where they willl be copied to dist.

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,26 @@ var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
-var minifyCSS = require('gulp-minify-css');
-var livereload = require('gulp-livereload');
+var minifyCSS = require('gulp-clean-css');
 var imagemin = require('gulp-imagemin');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
+var connect = require('gulp-connect');
+
 
 // callback function for error logging
 function errorLog (error) {
   console.error.bind(error);
   this.emit('end');
 }
+
+// html task
+// copies html files to the dist folder
+gulp.task('htmlCopy', function () {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'))
+})
 
 // scripts task
 // browserifies and uglifies js file
@@ -24,23 +32,24 @@ gulp.task('scripts', function() {
     .pipe(uglify())
     .on('error', console.error.bind(console))
     .pipe(gulp.dest('dist'))
+    .pipe(connect.reload())
 })
 
 // styles task
 // converts sass to css and compresses it also reloads server
 gulp.task('styles', function() {
   gulp.src('src/scss/**/*.scss')
-    .pipe(sourcemaps.init())
+    //.pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: 'compressed'
     })
     .on('error', sass.logError)) //.on('error', console.error.bind(console))
-    .pipe(sourcemaps.write('./maps'))
+    //.pipe(sourcemaps.write('./maps'))
     .pipe(postcss([
       autoprefixer({"browsers": ['last 2 versions']})
     ]))
     .pipe(gulp.dest('dist'))
-    .pipe(livereload())
+    .pipe(connect.reload())
 })
 
 // image task
@@ -55,12 +64,13 @@ gulp.task('image', function() {
 // watches all tasks
 gulp.task('watch', function() {
 
-  livereload.listen();﻿ // localhost:3000 and livereload button
+  connect.server({livereload: true});
 
+  gulp.watch('src/*.html', ['htmlCopy'])
   gulp.watch('src/js/*.js', ['scripts'])
   gulp.watch('src/scss/**/*.scss', ['styles']);
 })
 
 // default task
 // starts all tasks
-gulp.task('default', ['scripts', 'styles', 'watch']);
+gulp.task('default', ['htmlCopy','scripts', 'styles', 'watch']);
